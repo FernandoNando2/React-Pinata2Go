ec0454edf92eedaa41e221a4072b8757f8329ec3
FernandoNando2 <ferhdom@gmail.com>
2024-05-17 12:25:28 -0600

    Footer React Component

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>© ITQ 2024. Todos los derechos reservados.</p>
+        <ul>
+            <li>Terminos del Servicio</li>
+            <li>Politica de Privacidad</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Footer
